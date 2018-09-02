5956a1a8aafdfaf6d0d72e513b13e181cc5e791f
lucylucy5 <lizhongyi1990@gmail.com>
2018-09-02 10:49:38 -0400

    testthat updated

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,8 @@
 library(testthat)
 library(fars)
+library(readr)
 
 test_that("Data frame imported", {
-  library(readr)
   dat <- fars_read(readr_example("mtcars.csv"))
   expect_that(dat, is_a('data.frame'))
 })
